6da6ff5fdb5908efb88d2d823083531511026d42
gargvishu2000 <45060213+gargvishu2000@users.noreply.github.com>
2025-09-19 17:12:57 +0530

    added logout and checkAuth middleware

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.schema.js";
 import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 export const signUp = async(req,res)=>{
     
@@ -15,20 +16,25 @@ export const signUp = async(req,res)=>{
 
 export const signIn = async(req,res)=>{
     const {email,password} = req.body;
-    const user = User.findOne({email});
+    const user =await User.findOne({email});
     if(!user){
         return res.status(400).json({error: "No user found with this Email ID" })
     }
     const storedPassword = user.password;
-    const isValidPassword = await bcrypt.compare(storedPassword,password);
+    const isValidPassword = await bcrypt.compare(password,storedPassword);
 
     if(!isValidPassword){
         return res.status(400).json({error:"Credentials Invalid"})
     }else{
-        const token = jwt.sign({email:user.email,password:user.password}, JWT_SECRET,{expiresIn:'1h'})
-        res.cookie(token)
+        const token = jwt.sign({email:user.email,password:user.password}, process.env.JWT_SECRET,{expiresIn:'1h'})
+        res.cookie("token", token, {httpOnly:true, secure:true, maxAge: 60*60*1000})
         return res.status(201).json({message:"Login successful."})
     }
+}
+
+export const logOut = async(req,res)=>{
+    res.clearCookie("token",{httpOnly:true, secure:true})
+    return res.status(200).json({message:"Logout successfully"})
 }
 
 
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ dotenv.config()
 
 import express from 'express';
 import connectDB from './config/mongoose.js';
-import { signUp } from "./controller/auth.controller.js";
+import { signIn, signUp,logOut } from "./controller/auth.controller.js";
 import bodyParser from "body-parser"
 import cookieParser from "cookie-parser";
 import checkAuth from "./middlewares/auth.middleware.js";
@@ -14,7 +14,8 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.post('/api/signup', signUp);
-// app.post('/api/signin', singIn);
+app.post('/api/signin', signIn);
+app.get('/api/logout', logOut)
 
 app.get('/',checkAuth, (req,res)=>{
     console.log("HMS");
